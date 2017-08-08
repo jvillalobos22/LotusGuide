3f19a7b178bdaed77cc9b228d0978e5139642ee0
Juan Villalobos <juan.villalobos1992@gmail.com>
2017-08-08 16:38:11 -0700

    Changed user redirect function for new BusinessDirectory functionality testing

--- assets/functions/user-redirects.php
+++ assets/functions/user-redirects.php
@@ -1,18 +1,20 @@
 <?php
 
-function community_events_redirect( $url, $request, $user ){
-    if( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
-        if( $user->has_cap( 'administrator') or $user->has_cap( 'author')) {
-            $url = admin_url();
-        } else {
-            $url = home_url('/events/');
-        }
-    }
-    return $url;
-}
-add_filter('login_redirect', 'community_events_redirect', 10, 3 );
+// function community_events_redirect( $url, $request, $user ){
+//     if( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
+//         if( $user->has_cap( 'administrator') or $user->has_cap( 'author')) {
+//             $url = admin_url();
+//         } else {
+//             $url = admin_url();
+//             // $url = home_url('/events/');
+//         }
+//     }
+//     return $url;
+// }
+// add_filter('login_redirect', 'community_events_redirect', 10, 3 );
 
-if (!current_user_can('edit_posts')) {
-	add_filter('show_admin_bar', '__return_false');
+if (current_user_can('edit_posts')) {
+	add_filter('show_admin_bar', '__return_true');
+    // add_filter('show_admin_bar', '__return_false');
 }
 ?>
